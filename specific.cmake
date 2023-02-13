89e9dc7671e211f7e9f3b262af8e098d73d3fcc1
Wai-Shing Luk <luk036@gmail.com>
2023-02-13 10:48:21 +0800

    apply cmake-format

--- specific.cmake
+++ specific.cmake
@@ -1,31 +1,19 @@
-cpmaddpackage(
-  NAME
-  fmt
-  GIT_TAG
-  7.1.3
-  GITHUB_REPOSITORY
-  fmtlib/fmt
-  OPTIONS
-  "FMT_INSTALL YES" # create an installable target
+CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
-cpmaddpackage("gh:microsoft/GSL@3.1.0")
+CPMAddPackage("gh:microsoft/GSL@3.1.0")
 set(SPECIFIC_LIBS fmt::fmt GSL)
 
-# cpmaddpackage(
-#   NAME GSL
-#   GITHUB_REPOSITORY "microsoft/GSL"
-#   GIT_TAG "v4.0.0"
-#   GIT_SHALLOW ON
-# )
+# cpmaddpackage( NAME GSL GITHUB_REPOSITORY "microsoft/GSL" GIT_TAG "v4.0.0" GIT_SHALLOW ON )
 
 # include(FetchContent)
 #
-# FetchContent_Declare(GSL
-#     GIT_REPOSITORY "https://github.com/microsoft/GSL"
-#     GIT_TAG "v4.0.0"
-#     GIT_SHALLOW ON
-# )
+# FetchContent_Declare(GSL GIT_REPOSITORY "https://github.com/microsoft/GSL" GIT_TAG "v4.0.0"
+# GIT_SHALLOW ON )
 
 # FetchContent_MakeAvailable(GSL)
 
