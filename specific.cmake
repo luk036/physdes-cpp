bab7b073d14bc801115984e4a35704195afe1a43
Wai-Shing Luk <luk036@gmail.com>
2023-02-13 10:15:43 +0800

    update library version

--- specific.cmake
+++ specific.cmake
@@ -9,6 +9,24 @@ cpmaddpackage(
   "FMT_INSTALL YES" # create an installable target
 )
 
-cpmaddpackage("gh:microsoft/GSL@3.1.0")
+#cpmaddpackage("gh:microsoft/GSL@3.1.0")
+# cpmaddpackage(
+#   NAME GSL
+#   GITHUB_REPOSITORY "microsoft/GSL"
+#   GIT_TAG "v4.0.0"
+#   GIT_SHALLOW ON
+# )
 
-set(SPECIFIC_LIBS fmt::fmt GSL)
+# include(FetchContent)
+#
+# FetchContent_Declare(GSL
+#     GIT_REPOSITORY "https://github.com/microsoft/GSL"
+#     GIT_TAG "v4.0.0"
+#     GIT_SHALLOW ON
+# )
+
+# FetchContent_MakeAvailable(GSL)
+
+find_package(Microsoft.GSL CONFIG REQUIRED)
+
+set(SPECIFIC_LIBS fmt::fmt Microsoft.GSL::GSL)
