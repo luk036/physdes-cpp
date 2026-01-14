f201a94cd3d2fb0b85261d42a955ac6e24d2b736
Wai-Shing Luk <luk036@gmail.com>
2026-01-14 22:37:51 +0800

    no standalone main

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,4 +1,4 @@
-#include <recti/greeter.h>
+// #include <recti/greeter.h>
 #include <recti/version.h>
 
 #include <cxxopts.hpp>
@@ -6,45 +6,45 @@
 #include <string>
 #include <unordered_map>
 
-auto main(int argc, char** argv) -> int {
-    const std::unordered_map<std::string, recti::LanguageCode> languages{
-        {"en", recti::LanguageCode::EN},
-        {"de", recti::LanguageCode::DE},
-        {"es", recti::LanguageCode::ES},
-        {"fr", recti::LanguageCode::FR},
-    };
+auto main(int , char** ) -> int {
+    // const std::unordered_map<std::string, recti::LanguageCode> languages{
+    //     {"en", recti::LanguageCode::EN},
+    //     {"de", recti::LanguageCode::DE},
+    //     {"es", recti::LanguageCode::ES},
+    //     {"fr", recti::LanguageCode::FR},
+    // };
 
-    const cxxopts::Options options(*argv, "A program to welcome the world!");
+//     const cxxopts::Options options(*argv, "A program to welcome the world!");
 
-    std::string language;
-    std::string name;
+//     std::string language;
+//     std::string name;
 
-    // clang-format off
-  options.add_options()
-    ("h,help", "Show help")
-    ("v,version", "Print the current version number")
-    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
-  ;
-    // clang-format on
+//     // clang-format off
+//   options.add_options()
+//     ("h,help", "Show help")
+//     ("v,version", "Print the current version number")
+//     ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
+//     ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
+//   ;
+//     // clang-format on
 
-    const auto result = options.parse(argc, argv);
+//     const auto result = options.parse(argc, argv);
 
-    if (result["help"].as<bool>()) {
-        std::cout << options.help() << std::endl;
-        return 0;
-    }
+//     if (result["help"].as<bool>()) {
+//         std::cout << options.help() << std::endl;
+//         return 0;
+//     }
 
-    if (result["version"].as<bool>()) {
-        std::cout << "Recti, version " << RECTI_VERSION << std::endl;
-        return 0;
-    }
+//     if (result["version"].as<bool>()) {
+//         std::cout << "Recti, version " << RECTI_VERSION << std::endl;
+//         return 0;
+//     }
 
-    const auto langIt = languages.find(language);
-    if (langIt == languages.end()) {
-        std::cerr << "unknown language code: " << language << std::endl;
-        return 1;
-    }
+    // const auto langIt = languages.find(language);
+    // if (langIt == languages.end()) {
+    //     std::cerr << "unknown language code: " << language << std::endl;
+    //     return 1;
+    // }
 
     // recti::Recti recti(name);
     // std::cout << recti.greet(langIt->second) << std::endl;
