29deec490069d56c5b91e38870790fd42dd2c406
Wai-Shing Luk <luk036@gmail.com>
2026-08-15 18:39:28 +0800

    build: replace LdsGen with Lds and skip system fmt/spdlog on MSVC

--- xmake.lua
+++ xmake.lua
@@ -46,7 +46,7 @@ target("test_recti")
 set_kind("binary")
 add_deps("Recti")
 add_includedirs("include", { public = true })
-add_includedirs("../lds-gen-cpp/include", { public = true })
+add_includedirs("../lds-cpp/include", { public = true })
 add_files("test/source/*.cpp")
 add_packages("doctest", "fmt", "spdlog")
 
